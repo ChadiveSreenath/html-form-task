d8b376233261705d1d2ac051a1cce5d623dd19b6
Chadivesreenath <sreenath.chadive@gmail.com>
2023-05-07 04:23:34 +0530

    post request to Airtable db

--- index.js
+++ index.js
@@ -6,29 +6,43 @@ let number = document.getElementById('number');
 let gender = document.getElementById('gender');
 
 
+const apiToken = 'patic68uHmCo4HxM5.03ae8ae480e6c7f44c1ec5b7465cfc3a0338862528aa0aff82bd86adc5931264'
 
+const PostReq = (formData) => {
+    fetch(` https://api.airtable.com/v0/app7p1MFqcBHtIwfh/tableTask`, {
+        method: 'POST',
+        body: JSON.stringify({
+            "records": [
+                {
+                    "fields": formData
+                }
+            ]
+        }),
+        headers: {
+            "Authorization": `Bearer ${apiToken}`,
+            "Content-Type": "application/json"
+        }
+    }).then((response) => console.log(response))
+        .catch((error) => console.log(error))
+}
 
-document.getElementById('submit').addEventListener('click', (e) => {
-    e.preventDefault();
-    addData()
-})
-
-
-const addData = (e) => {
-    const dataObj = {
-        name: username.value,
-        email: email.value,
-        number: number.value,
-        gender: gender.value
-    }
-
+const addData = (event) => {
+    event.preventDefault();
     if (username.value === '' || email.value === '' || number.value === '' || gender.value === '') {
         alert('Please enter all fiels')
         return
     }
     else {
-        alert('thanks for filling the form')
+        alert('thanks for filling the form data is added to Airtable')
+        const formData = {
+            'name': username.value,
+            'email': email.value,
+            'number': number.value,
+            'gender': gender.value
+        }
+        PostReq(formData)
         document.getElementById("myForm").reset()
-        console.log(dataObj)
     }
 }
+
+document.getElementById('submit').addEventListener('click', addData)
